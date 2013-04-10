ec6af86fceffdeeb409152c8df5ae4e210caa732
svelikov <svelikov@10.131.2.196>
2013-04-10 14:31:33 +0300

    first import

---
+++ gittestproj/src/main/java/com/test/Test.java
@@ -0,0 +1,17 @@
+package com.test;
+
+/**
+ * The Class Test.
+ */
+public class Test {
+
+	/**
+	 * The main method.
+	 * 
+	 * @param args
+	 *            the arguments
+	 */
+	public static void main(String[] args) {
+		System.out.println("hi");
+	}
+}
